a69ff9f57bd9d048176e3d68ace4c5db7d2d2b6d
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-11-04 22:09:49 +0100

    integrated fully reader component

--- code-editor/editor/src/components/reader.js
+++ code-editor/editor/src/components/reader.js
@@ -1,9 +1,27 @@
+import ViewBuilder from "../classes/ViewBuilder.js"
+
 class CusomReader extends HTMLElement {
     constructor() {
         super()
+        this.viewBuilder = new ViewBuilder(this)
         this.writer = document.getElementById('writer')
         this.writer.addEventListener('input', (event) => {
-            this.innerHTML = event.target.value
+            const content = String(event.target.value)
+            const lines = content.split('\n')
+            this.viewBuilder.changesTracker.defineTheNewFirstLineOfChange(event.target.selectionStart, event.target.value)
+            this.viewBuilder.buildCodeForLines(lines)
+        })
+
+        this.writer.addEventListener('scroll', () => {
+            this.scrollTop = this.writer.scrollTop
+            this.scrollLeft = this.writer.scrollLeft
+        })
+
+        this.writer.addEventListener('selectionchange', (event) => {
+            const startingIndex = event.target.selectionStart
+            const endingIndex = event.target.selectionEnd
+            this.viewBuilder.changesTracker.defineTheFirstLineOfChange(startingIndex, event.target.value)
+            this.viewBuilder.changesTracker.defineTheLastLineOfChange(endingIndex, event.target.value)
         })
     }
 }

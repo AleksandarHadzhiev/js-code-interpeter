38e6a7f44a95c86e660bbb9ed2f42eaed55803e3
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-11-04 21:37:54 +0100

    track cursor position in the textarea

---
+++ code-editor/editor/src/classes/PositionChangesTracker.js
@@ -0,0 +1,27 @@
+export default class PositionChangesTracker {
+    constructor() {
+        this.firstLineOfChange = 0;
+        this.lastLineOfChange = 0;
+        this.newFirstLineOfChange = 0;
+    }
+
+    defineTheFirstLineOfChange(index, content) {
+        this.firstLineOfChange = this._defineLineOfChange(index, content)
+    }
+
+    defineTheLastLineOfChange(index, content) {
+        this.lastLineOfChange = this._defineLineOfChange(index, content)
+    }
+
+    defineTheNewFirstLineOfChange(index, content) {
+        this.newFirstLineOfChange = this._defineLineOfChange(index, content)
+    }
+
+    _defineLineOfChange(index, content) {
+        let position = index
+        if (index == 0)
+            position = 1
+        const substringed = String(content).substring(0, position);
+        return substringed.split('\n').length
+    }
+}

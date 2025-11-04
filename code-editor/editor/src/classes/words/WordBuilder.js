3ab86f6cd36641a5fde3bcf43635ae9cf90355a7
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-11-04 14:22:39 +0100

    refactor word builder

--- code-editor/editor/src/classes/words/WordBuilder.js
+++ code-editor/editor/src/classes/words/WordBuilder.js
@@ -1,50 +1,41 @@
-import WordsSplitter from "./WordsSplitter.js";
 import WordColorDefiner from "./WordColorDefiner.js";
 
 // WordBuilder Does More than one thing
-// He splits the line into words
 // He creates the words
 // He combines them into one whole line and returns them to the line...
 // SPLIT THE RESPONSIBILITIES
 export default class WordBuilder {
     constructor() {
-        this.splitter = new WordsSplitter();
         this.color = "transparent"
         this.colorDefiner = null;
+        this.commentInit = 0;
     }
 
-    buildWordsForLineAsHTML(line) {
-        const words = this.splitter.splitLineIntoWords(line)
-        const innterHTML = this._generateWordsAsHTMLElements(words)
-        return innterHTML
-    }
-
-    _generateWordsAsHTMLElements(words) {
-        if (words.length == 1 && words[0].trim() == "") return `<span style="font-sieze: 24px">&nbsp;</span>`
-        else return this._generateForMultiple(words)
-    }
-
-    _generateForMultiple(words) {
-        let innerHTML = ''
-        let initComment = 0;
+    buildWordAtIndexInWordsAsHTML(word, index, words) {
+        this._trackCommentSection(word)
         this.colorDefiner = new WordColorDefiner(words)
-        words.forEach((word, index) => {
-            if (word == "/")
-                initComment += 1;
-            if (word == " ") {
-                innerHTML += `<span style="font-sieze: 24px">&nbsp;</span>`
-            }
-            else if (initComment >= 2) {
-                innerHTML += `<span style="font-sieze: 24px; color: gray">${word}</span>`
-            }
-            else if (word == '\t') {
-                innerHTML += `<span style="font-sieze: 24px">&emsp;</span>`
-            }
-            else {
-                this.color = this.colorDefiner.defineColorForWordAtIndex(word, index)
-                innerHTML += `<span style="font-sieze: 24px; color: ${this.color}">${word}</span>`
-            }
-        });
-        return innerHTML
+        return this._buildWordElementForWordAtIndex(word, index)
+    }
+
+    _trackCommentSection(word) {
+        if (word == "/")
+            this.commentInit += 1
+        else this.commentInit = 0
+    }
+
+    _buildWordElementForWordAtIndex(word, index) {
+        if (word == " ") {
+            return `<span style="font-sieze: 24px">&nbsp;</span>`
+        }
+        else if (this.commentInit >= 2) {
+            return `<span style="font-sieze: 24px; color: gray">${word}</span>`
+        }
+        else if (word == '\t') {
+            return `<span style="font-sieze: 24px">&emsp;</span>`
+        }
+        else {
+            this.color = this.colorDefiner.defineColorForWordAtIndex(word, index)
+            return `<span style="font-sieze: 24px; color: ${this.color}">${word}</span>`
+        }
     }
 }

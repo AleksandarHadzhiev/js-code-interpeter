d0c0c48544e5b2cd84b4e28f96113afa165ab395
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-11-05 08:37:41 +0100

    search: better styling

--- code-editor/editor/src/components/SearchAndReplace/search.js
+++ code-editor/editor/src/components/SearchAndReplace/search.js
@@ -1,6 +1,14 @@
 class Search extends HTMLElement {
     constructor() {
         super()
+        const container = this._buildMainContainer()
+        this.appendChild(container)
+    }
+
+    _buildMainContainer() {
+        const mainContainer = document.createElement('div')
+        mainContainer.classList.add("search-container")
+        return mainContainer
     }
 }
 

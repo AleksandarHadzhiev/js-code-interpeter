68cffb4f7dade295c5f5def1e023d12aa60d6dbb
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-11-06 21:57:37 +0100

    search: switching between highlighted elements

--- code-editor/editor/src/components/SearchAndReplace/search.js
+++ code-editor/editor/src/components/SearchAndReplace/search.js
@@ -2,6 +2,7 @@ class Search extends HTMLElement {
     constructor() {
         super()
         this.foundElements = []
+        this.currentPosition = 1
         this.specialCharaters = ['(', ')', '[', ']', '{', '}']
         this.reader = document.getElementById('reader')
         const container = this._buildMainContainer()
@@ -23,7 +24,7 @@ class Search extends HTMLElement {
         searchBar.classList.add('search-bar')
         searchBar.placeholder = "Search for..."
         searchBar.addEventListener('input', (event) => {
-            this.foundElements = []
+            this.currentPosition = 0
             const content = String(event.target.value)
             const highlightedElements = document.getElementsByName('highlighted')
             while (highlightedElements.length > 0) {
@@ -37,8 +38,13 @@ class Search extends HTMLElement {
     }
 
     _updateInfo() {
+        const switcher = document.getElementById('position-switcher')
         const info = document.getElementById('info')
-        info.textContent = `${this.foundElements.length > 0 ? "1" : '0'} : ${this.foundElements.length}`;
+        if (this.foundElements.length > 0) {
+            switcher.classList.remove('hidden')
+            info.textContent = `${this.currentPosition + 1} : ${this.foundElements.length}`;
+        }
+        else info.textContent = `No elements found`;
     }
 
     _searchForContentInsideReader(content) {
@@ -85,7 +91,7 @@ class Search extends HTMLElement {
         let innerHtml = ""
         appearences.forEach((appearence, index) => {
             let initialText = this._generateInitialText(appearences, appearence, content, oldText, index)
-            const replaceText = `<span id="${lineIndex}-${wordIndex}-${index}" name= "highlighted" style="background-color: lightyellow">${oldText.substring(appearence, appearence + content.length)}</span>`
+            const replaceText = `<span id="${lineIndex}-${wordIndex}-${index}" name= "highlighted" class="highlighted">${oldText.substring(appearence, appearence + content.length)}</span>`
             this.foundElements.push(`${lineIndex}-${wordIndex}-${index}`)
             innerHtml += `${initialText}${replaceText}`
         });
@@ -104,6 +110,7 @@ class Search extends HTMLElement {
         const container = document.createElement('div')
         container.classList.add('found-elements')
         container.appendChild(this._buildInfoElement())
+        container.appendChild(this._buildSwitch())
         return container
     }
 
@@ -111,8 +118,60 @@ class Search extends HTMLElement {
         const info = document.createElement('p')
         info.setAttribute('id', 'info')
         info.classList.add('info')
-        info.innerHTML = `0 : ${this.foundElements.length} `
         return info
+    }
+
+    _buildSwitch() {
+        const switchContainer = document.createElement('div')
+        switchContainer.setAttribute('id', 'position-switcher')
+        switchContainer.classList.add('switch')
+        switchContainer.classList.add('hidden')
+        const goPrevious = this._buildButtonForAction("previous")
+        const goNext = this._buildButtonForAction("next")
+        switchContainer.appendChild(goPrevious)
+        switchContainer.appendChild(goNext)
+        return switchContainer
+    }
+
+    _buildButtonForAction(buttonAction) {
+        const button = document.createElement('button')
+        button.textContent = buttonAction == "previous" ? "<" : ">"
+        button.classList.add('position-switching-button')
+        let prevElement = null
+        button.addEventListener('click', () => {
+            if (prevElement) prevElement.classList.remove('currently-selected')
+            this._updatePositionBasedOnAction(buttonAction)
+            const element = document.getElementById(this.foundElements[this.currentPosition])
+            element.classList.add('currently-selected')
+            console.log(element)
+            element.scrollIntoView()
+            this._updateInfo()
+            prevElement = element
+        })
+        return button
+    }
+
+    _updatePositionBasedOnAction(action) {
+        if (action == "previous") this._updateCurrentPositionTowardsPrev()
+        else this._updateCurrentPositionTowardsNext()
+    }
+
+    _updateCurrentPositionTowardsNext() {
+        if (this.currentPosition == this.foundElements.length - 1) {
+            this.currentPosition = 0;
+        }
+        else {
+            this.currentPosition += 1;
+        }
+    }
+
+    _updateCurrentPositionTowardsPrev() {
+        if (this.currentPosition == 0) {
+            this.currentPosition = this.foundElements.length - 1;
+        }
+        else {
+            this.currentPosition -= 1;
+        }
     }
 }
 

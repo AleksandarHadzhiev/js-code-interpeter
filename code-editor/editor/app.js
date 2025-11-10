d6c31c767f7afd9f71e2d42631e9197e9ca1b9ff
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2025-11-10 09:33:08 +0100

    search: working with shortcut

--- code-editor/editor/app.js
+++ code-editor/editor/app.js
@@ -2,6 +2,8 @@ import LineNumberationTracker from "./src/classes/lineNumerationTracker.js"
 
 const writer = document.getElementById('writer')
 const editorContainer = document.getElementById('editor-container')
+const search = document.getElementById('search')
+const highlighter = document.getElementById('highlighter')
 
 // Start from line 1 when the app boots up
 new LineNumberationTracker().buildLineNumbers([])
@@ -15,4 +17,21 @@ writer.addEventListener('input', (event) => {
 writer.addEventListener('scroll', (event) => {
     editorContainer.scrollTop = event.target.scrollTop
     editorContainer.scrollLeft = event.target.scrollLeft
+    highlighter.scrollLeft = event.target.scrollLeft
+    highlighter.scrollTop = event.target.scrollTop
+})
+
+document.addEventListener('keydown', (event) => {
+
+    if (event.ctrlKey && event.key == "f" || event.key == "F") {
+        event.preventDefault()
+        if (search.classList.contains('invisible')) {
+            search.classList.replace('invisible', 'search')
+            highlighter.classList.replace('invisible', 'highlighter')
+        }
+        else {
+            search.classList.replace('search', 'invisible')
+            highlighter.classList.replace('highlighter', 'invisible')
+        }
+    }
 })

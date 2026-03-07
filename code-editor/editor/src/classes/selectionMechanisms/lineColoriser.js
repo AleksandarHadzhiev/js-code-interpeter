6f523ec193e6d6c27dfb5adb46f3f76d493d240d
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-03-07 10:23:04 +0100

    initialize class for line colorization

---
+++ code-editor/editor/src/classes/selectionMechanisms/lineColoriser.js
@@ -0,0 +1,30 @@
+class MarkedPoint {
+    /**
+ * 
+ * @param {Number} top 
+ * @param {Number} left 
+ * @param {Number} width 
+ * @param {Number} lineId 
+ */
+    constructor(top, left, width, lineId) {
+        this.top = top
+        this.left = left
+        this.lineId = lineId
+        this.width = width
+    }
+}
+
+export default class LineColoriser {
+    /**
+     * 
+     * @param {CustomRangeElement} startingPoint 
+     * @param {CustomRangeElement} endingPoint 
+     */
+    constructor(startingPoint, endingPoint) {
+        this.startingPoint = startingPoint
+        this.endingPoint = endingPoint
+        this.coordinatesToHighlight = new Map()
+        this.startingMarkedPoint = null
+        this.endingMarkedPoint = null
+    }
+}

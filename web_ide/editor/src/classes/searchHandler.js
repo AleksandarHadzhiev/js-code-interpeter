e5847e5d58042416d3282191c8c27a53f427efc1
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-06 14:14:46 +0200

    get the text from the search field

--- web_ide/editor/src/classes/searchHandler.js
+++ web_ide/editor/src/classes/searchHandler.js
@@ -1,10 +1,17 @@
-import { textToWokWith } from "../../textToWorkWith";
+import { textToWokWith } from "../../textToWorkWith.js";
 
 export default class SearchHandler {
     constructor() {
         this.search = document.getElementById('search-container')
+        this.searchField = document.getElementById('search-field')
         this.class = 'hidden'
         this.textToWokWith = textToWokWith
+
+        this.searchField.addEventListener('input', (event) => {
+            const textToSearchFor = this.searchField.value
+            // how to properly search for the text?
+            console.log(textToSearchFor)
+        })
     }
 
     changeVisibility() {

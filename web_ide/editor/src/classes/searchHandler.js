e09bac371fe62277719e05b6adce365b77d334e9
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-06 13:59:17 +0200

    swithc .txt to .js file

--- web_ide/editor/src/classes/searchHandler.js
+++ web_ide/editor/src/classes/searchHandler.js
@@ -1,10 +1,14 @@
+import { textToWokWith } from "../../textToWorkWith";
+
 export default class SearchHandler {
     constructor() {
         this.search = document.getElementById('search-container')
         this.class = 'hidden'
+        this.textToWokWith = textToWokWith
     }
 
     changeVisibility() {
+        console.log(this.textToWokWith)
         this.class = this.class == "hidden" ? "search-container" : "hidden"
         this.search.className = this.class
     }
--- web_ide/editor/textToWorkWith.js
+++ web_ide/editor/textToWorkWith.js
@@ -1,3 +1,4 @@
+export const textToWokWith = `
 function getName(age) { console.log("MEOW"); }
 This is a long line to be displayed, and for that reason it will have a lot of text inside it.
 function getName(age) { console.log("MEOW"); }
@@ -1998,3 +1999,5 @@ function getName(age) { console.log("MEOW"); }
 This is a long line to be displayed, and for that reason it will have a lot of text inside it.
 function getName(age) { console.log("MEOW"); }
 This is a long line to be displayed, and for that reason it will have a lot of text inside it.
+
+`

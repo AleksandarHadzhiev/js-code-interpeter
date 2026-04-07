5a1e86767f9310c310af33cb60446e4e7c28e579
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-07 21:40:59 +0200

    define positions of highlighters on visible text

--- web_ide/editor/src/classes/searchHandler.js
+++ web_ide/editor/src/classes/searchHandler.js
@@ -1,10 +1,11 @@
 import { textToWokWith } from "../../textToWorkWith.js";
-import HighlightProvider from "./highlighters/HighlightProvider.js";
+import calculateWidthForText from "./calculators/widthOfTextCalculator.js";
 
 export default class SearchHandler {
     constructor() {
         this.search = document.getElementById('search-container')
         this.searchField = document.getElementById('search-field')
+        this.lineContent = document.getElementById('line-content')
         this.infoForAmountOfAppearencesOfText = document.getElementById(`info-highlighted-lines`)
         this.class = 'hidden'
         this.amountOfAppearences = "No results"
@@ -32,13 +33,19 @@ export default class SearchHandler {
     }
 
     _higlightTextDifferentThanEmpty(textToSearchFor) {
-        this._singleLineHighlight(textToSearchFor).then((numberOfAppearences) => {
-            this._updateInfo(numberOfAppearences)
-        })
-
+        this._singleLineHighlighter(textToSearchFor)
     }
 
-    _singleLineHighlight(textToSearchFor) {
+    _singleLineHighlighter(textToSearchFor) {
+        this._singleLineHighlightCheckInFullText(textToSearchFor).then((numberOfAppearences) => {
+            this._updateInfo(numberOfAppearences)
+        })
+        this._highlightTextVisibleOnScreen(textToSearchFor).then((highlighters) => {
+            this._buildHighlighters(highlighters)
+        })
+    }
+
+    _singleLineHighlightCheckInFullText(textToSearchFor) {
         return new Promise(function (resolve, reject) {
             let amountOfAppearences = 0
             try {
@@ -55,7 +62,47 @@ export default class SearchHandler {
     }
 
     _updateInfo(numberOfAppearences) {
+        console.log(numberOfAppearences)
         this.amountOfAppearences = `0 of ${numberOfAppearences}`
         this.infoForAmountOfAppearencesOfText.textContent = this.amountOfAppearences
+    }
+
+    /**
+     * 
+     * @param {String} textToSearchFor 
+     * @returns 
+     */
+    _highlightTextVisibleOnScreen(textToSearchFor) {
+        const lines = this.lineContent.childNodes
+        return new Promise(function (resolve, reject) {
+            const highlighters = []
+            try {
+                lines.forEach((lineELement) => {
+                    const parent = lineELement.parentElement
+                    const lineText = lineELement.textContent
+                    const topOffset = lineELement.id * 28.8
+                    const matchesOnLine = lineText.matchAll(textToSearchFor)
+                    matchesOnLine.forEach((match, index) => {
+                        const text = lineText.substring(0, match.index)
+                        const textForWidth = lineText.substring(match.index, match.index + textToSearchFor.length)
+                        const leftOffset = calculateWidthForText(parent, text)
+                        const width = calculateWidthForText(parent, textForWidth)
+                        highlighters.push({ "width": width, "left": leftOffset, "top": topOffset })
+                    })
+                })
+                resolve(highlighters)
+            }
+            catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    /**
+     * 
+     * @param {Array} highlighters 
+     */
+    _buildHighlighters(highlighters) {
+        console.log(highlighters)
     }
 }

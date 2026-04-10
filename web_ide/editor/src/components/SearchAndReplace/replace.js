92b0affd0969d6f0a626b1ebe645fb73006c0193
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-10 21:28:00 +0200

    buttons for replacing text

--- web_ide/editor/src/components/SearchAndReplace/replace.js
+++ web_ide/editor/src/components/SearchAndReplace/replace.js
@@ -7,7 +7,9 @@ export default class CustomReplaceContainer {
         replaceContainer.className = 'replace-container'
         replaceContainer.setAttribute('id', 'replace-container')
         const replaceField = this._buildReplaceTextField()
+        const replaceActionsContainer = this._replaceOperationsContainer()
         replaceContainer.appendChild(replaceField)
+        replaceContainer.appendChild(replaceActionsContainer)
         return replaceContainer
     }
 
@@ -17,5 +19,31 @@ export default class CustomReplaceContainer {
         replaceField.setAttribute('id', 'replace-field')
         replaceField.setAttribute('placeholder', "Replace")
         return replaceField
+    }
+
+    _replaceOperationsContainer() {
+        const container = document.createElement('div')
+        container.classList.add('replace-actions-container')
+        const replaceOne = this._replaceOne()
+        const replaceAll = this._replaceAll()
+        container.appendChild(replaceOne)
+        container.appendChild(replaceAll)
+        return container
+    }
+
+    _replaceOne() {
+        const replaceOne = document.createElement('button')
+        replaceOne.setAttribute('id', 'replace-one')
+        replaceOne.className = "replace-button"
+        replaceOne.textContent = "1"
+        return replaceOne
+    }
+
+    _replaceAll() {
+        const replaceAll = document.createElement('button')
+        replaceAll.setAttribute('id', 'replace-all')
+        replaceAll.className = "replace-button"
+        replaceAll.textContent = "All"
+        return replaceAll
     }
 }

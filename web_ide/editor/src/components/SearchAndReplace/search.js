00e68573c65c040e5dd5ed872af3c6046289be6b
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-11 09:50:28 +0200

    add switch buttons

--- web_ide/editor/src/components/SearchAndReplace/search.js
+++ web_ide/editor/src/components/SearchAndReplace/search.js
@@ -8,8 +8,10 @@ export default class CustomSearchContainer {
         searchContainer.className = 'search-container'
         const searchField = this._buildSearchTextField()
         const info = this._buildInfo()
+        const switchContainer = this._buildSwitchContainer()
         searchContainer.appendChild(searchField)
         searchContainer.appendChild(info)
+        searchContainer.appendChild(switchContainer)
         return searchContainer
     }
 
@@ -27,5 +29,31 @@ export default class CustomSearchContainer {
         info.setAttribute('id', 'info-highlighted-lines')
         info.textContent = "No results"
         return info
+    }
+
+    _buildSwitchContainer() {
+        const container = document.createElement('div')
+        container.className = 'switch-container'
+        const goUp = this._buildGoUp()
+        const goDown = this._buildGoDown()
+        container.appendChild(goUp)
+        container.appendChild(goDown)
+        return container
+    }
+
+    _buildGoUp() {
+        const goUpButton = document.createElement('button')
+        goUpButton.setAttribute('id', 'go-up')
+        goUpButton.className = 'switch-button'
+        goUpButton.textContent = "<"
+        return goUpButton
+    }
+
+    _buildGoDown() {
+        const goDownButton = document.createElement('button')
+        goDownButton.setAttribute('id', 'go-down')
+        goDownButton.className = 'switch-button'
+        goDownButton.textContent = ">"
+        return goDownButton
     }
 }

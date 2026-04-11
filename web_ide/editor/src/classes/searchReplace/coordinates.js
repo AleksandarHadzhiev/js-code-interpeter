e742ccf73d57901b9d773ffbb2407a152bb0a408
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-11 20:50:21 +0200

    extract coordinates from searchHandler

---
+++ web_ide/editor/src/classes/searchReplace/coordinates.js
@@ -0,0 +1,13 @@
+export default class Coordinates {
+    /**
+     * 
+     * @param {Number} width 
+     * @param {Number} left 
+     * @param {Number} top 
+     */
+    constructor(width, left, top) {
+        this.width = width
+        this.left = left
+        this.top = top
+    }
+}

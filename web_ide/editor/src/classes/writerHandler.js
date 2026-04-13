f6902c1e13a5dc7e7343e48bda93e70cc19faad1
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-13 21:38:15 +0200

    fix: removing text

--- web_ide/editor/src/classes/writerHandler.js
+++ web_ide/editor/src/classes/writerHandler.js
@@ -2,6 +2,7 @@ import SearchReplaceHandler from "./searchReplace/searchReplaceHandler.js"
 import { findCaretCurrentPositionInText } from "./calculators/caretLeftOffsetCalculator.js"
 import CaretBuilder from "./selectionMechanisms/caretBuilder.js"
 import turnWidthToIndexForText from "./calculators/offsetToTextCalculator.js"
+import calculateWidthForText from "./calculators/widthOfTextCalculator.js"
 
 export default class WriterHandler {
     /**
@@ -22,43 +23,12 @@ export default class WriterHandler {
     removeText() {
         let indexInText = findCaretCurrentPositionInText(this.caret, this.textToWorkWith, this.contentElement)
         let newIndexForCaret = indexInText
-        const isStartOfLine = this._checkIfAtStartOfLine()
-        if (isStartOfLine == false) {
-            const textBefore = this.textToWorkWith.substring(0, indexInText - 2)
-            const textAfter = this.textToWorkWith.substring(indexInText - 1, this.textToWorkWith.length)
-            newIndexForCaret = indexInText - 2
-            this.textToWorkWith = `${textBefore}${textAfter}`
-            this.searchReplaceHandler.updateText(this.textToWorkWith)
-            this._moveCaretOneIndexFurther(newIndexForCaret)
-        }
-        else {
-            newIndexForCaret = this._deleteLineWithPassingContentToThePrevLine()
-            this.searchReplaceHandler.updateText(this.textToWorkWith)
-            this._moveCaretOneIndexFurther(newIndexForCaret)
-        }
-    }
-
-    _checkIfAtStartOfLine() {
-        if (this.caret.offsetLeft == 0) {
-            return true
-        }
-        return false
-    }
-
-    _deleteLineWithPassingContentToThePrevLine(indexInText) {
-        const lines = this.textToWorkWith.split('\n')
-        const lineId = Math.round(this.caret.offsetTop / 28.8)
-        let index = indexInText
-        if (lineId > 0 && lineId <= lines.length - 1) {
-            const previousLine = lineId - 1
-            const textOnPrevLine = lines[previousLine]
-            const textOnCurrLine = lines[lineId]
-            lines[previousLine] = `${textOnPrevLine}${textOnCurrLine}`
-            lines.splice(lineId, 1)
-            this.textToWorkWith = lines.join('\n')
-            index = String(textOnPrevLine).length
-        }
-        return index
+        const textBefore = this.textToWorkWith.substring(0, indexInText - 1)
+        const textAfter = this.textToWorkWith.substring(indexInText, this.textToWorkWith.length)
+        newIndexForCaret = indexInText - 1
+        this.textToWorkWith = `${textBefore}${textAfter}`
+        this.searchReplaceHandler.updateText(this.textToWorkWith)
+        this._moveCaretOneIndexFurther(newIndexForCaret)
     }
 
     /**

4613d134d08f36048c2cb3841e91111533e65c89
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-14 08:10:00 +0200

    fix: single line text selection change of text

--- web_ide/editor/src/classes/selectionMechanisms/textFetcher.js
+++ web_ide/editor/src/classes/selectionMechanisms/textFetcher.js
@@ -14,6 +14,8 @@ export default class TextFetcher {
         this.startingIndex = 0
         this.endingIndex = 0
         this.text = ""
+        this.textToWorkWith = ""
+        this.lines = []
     }
 
     /**
@@ -21,14 +23,15 @@ export default class TextFetcher {
      * @param {String} fullText 
      */
     selectTextFromFullText(fullText) {
-        const lines = fullText.split('\n')
+        this.textToWorkWith = fullText
+        this.lines = fullText.split('\n')
         const lineForEndingPoint = this.highlighter.endingPoint.lineId
         const lineForStartingPoint = this.highlighter.startingPoint.lineId
         if (lineForStartingPoint == lineForEndingPoint) {
             this._singleLineTextSelection()
         }
         else {
-            this._multilineTextSelection(lines, lineForStartingPoint, lineForEndingPoint)
+            this._multilineTextSelection(lineForStartingPoint, lineForEndingPoint)
         }
         return {
             "text": this.text,
@@ -43,39 +46,60 @@ export default class TextFetcher {
         const fullTextWidth = calculateWidthForText(this.contentElement, fullText)
         const startingIndex = turnWidthToIndexForText(this.highlighter.startingPoint.leftOffset, fullTextWidth, fullText.length)
         const endingIndex = turnWidthToIndexForText(this.highlighter.endingPoint.leftOffset, fullTextWidth, fullText.length)
-        this.startingIndex = startingIndex
-        this.endingIndex = endingIndex
+        this._setPointsFor(startingIndex, endingIndex)
         this.text = fullText.substring(startingIndex, endingIndex)
     }
 
     /**
      * 
-     * @param {Array} lines 
-     * @param {Number} lineForStartingPoint 
-     * @param {Number} lineForEndingPoint 
+     * @param {Number} startingIndex 
+     * @param {Number} endingIndex 
      */
-    _multilineTextSelection(lines, lineForStartingPoint, lineForEndingPoint) {
-        if (lineForEndingPoint > lineForStartingPoint) {
-            return this._startingLineFirst(lines, lineForStartingPoint, lineForEndingPoint)
+    _setPointsFor(startingIndex, endingIndex) {
+        const textInLinesTillFirst = this.lines.slice(0, this.highlighter.startingPoint.lineId)
+        const textInLinesTillLast = this.lines.slice(0, this.highlighter.endingPoint.lineId)
+        const textFirst = textInLinesTillFirst.join('\n')
+        const textLast = textInLinesTillLast.join('\n')
+        if (this.highlighter.startingPoint.lineId > 0) {
+            this.startingIndex = startingIndex + textFirst.length + 1
+            if (this.highlighter.endingPoint > 0)
+                this.endingIndex = endingIndex + textLast.length + 1
+            else this.endingIndex = endingIndex + textLast.length
         }
         else {
-            return this._endingLineFirst(lines, lineForEndingPoint, lineForStartingPoint)
+            this.startingIndex = startingIndex + textFirst.length
+            if (this.highlighter.endingPoint > 0)
+                this.endingIndex = endingIndex + textLast.length + 1
+            else this.endingIndex = endingIndex + textLast.length
         }
     }
 
     /**
      * 
-     * @param {Array} lines 
+     * @param {Number} lineForStartingPoint 
+     * @param {Number} lineForEndingPoint 
+     */
+    _multilineTextSelection(lineForStartingPoint, lineForEndingPoint) {
+        if (lineForEndingPoint > lineForStartingPoint) {
+            return this._startingLineFirst(lineForStartingPoint, lineForEndingPoint)
+        }
+        else {
+            return this._endingLineFirst(lineForEndingPoint, lineForStartingPoint)
+        }
+    }
+
+    /**
+     * 
      * @param {Number} firstLine 
      * @param {Number} lastLine 
      * @returns 
      */
-    _startingLineFirst(lines, firstLine, lastLine) {
+    _startingLineFirst(firstLine, lastLine) {
         const selectedTextLines = []
         const textOfFirstLine = this._getTextForFirstSelectedLine(this.highlighter.startingPoint)
         const textOfEndingLine = this._getOfLastLine(this.highlighter.endingPoint)
         selectedTextLines.push(textOfFirstLine)
-        this._getLinesInBetween(lines, firstLine + 1, lastLine, selectedTextLines)
+        this._getLinesInBetween(firstLine + 1, lastLine, selectedTextLines)
         selectedTextLines.push(textOfEndingLine)
         this.text = selectedTextLines.join('\n')
     }
@@ -98,30 +122,28 @@ export default class TextFetcher {
 
     /**
      * 
-     * @param {Array} lines 
      * @param {Number} firstLine 
      * @param {Number} lastLine 
      * @param {Array} linesText 
      */
-    _getLinesInBetween(lines, firstLine, lastLine, linesText) {
+    _getLinesInBetween(firstLine, lastLine, linesText) {
         for (let index = firstLine; index < lastLine; index++) {
-            const lineText = lines[index];
+            const lineText = this.lines[index];
             linesText.push(lineText)
         }
     }
 
     /**
-    * @param {Array} lines 
     * @param {Number} firstLine 
     * @param {Number} lastLine 
     * @returns 
     */
-    _endingLineFirst(lines, firstLine, lastLine) {
+    _endingLineFirst(firstLine, lastLine) {
         const selectedTextLines = []
         const textOfFirstLine = this._getTextForFirstSelectedLine(this.highlighter.endingPoint)
         const textOfEndingLine = this._getOfLastLine(this.highlighter.startingPoint)
         selectedTextLines.push(textOfFirstLine)
-        this._getLinesInBetween(lines, firstLine + 1, lastLine, selectedTextLines)
+        this._getLinesInBetween(firstLine + 1, lastLine, selectedTextLines)
         selectedTextLines.push(textOfEndingLine)
         this.text = selectedTextLines.join('\n')
     }

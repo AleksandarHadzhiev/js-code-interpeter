20ac39abfc05753caac9115d842bcd3590f6cb8f
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-15 14:19:14 +0200

    basic explorer added to the sidebar

--- web_ide/editor/src/components/menuContainer.js
+++ web_ide/editor/src/components/menuContainer.js
@@ -1,9 +1,35 @@
 class CustomMenuContainer extends HTMLElement {
     constructor() {
         super()
+        this.elements = []
+        const explorer = this._buildExplorer()
+        this.appendChild(explorer)
     }
 
+    _buildExplorer() {
+        const explorer = document.createElement('div')
+        explorer.setAttribute('id', 'explorer')
+        const icon = this._buildExplorerIcon()
+        explorer.appendChild(icon)
+        explorer.addEventListener('click', () => {
+            explorer.classList.add('selected')
+            this.elements.forEach((element) => {
+                if (element.classList.contains("selected") && element.id != 'explorer') {
+                    element.classList.remove('selected')
+                }
+            })
+        })
+        this.elements.push(explorer)
+        return explorer
+    }
 
+    _buildExplorerIcon() {
+        const icon = document.createElement('img')
+        icon.className = 'icon'
+        icon.src = "./icons/explorer.png"
+        icon.alt = 'Files explorer'
+        return icon
+    }
 }
 
 customElements.define('custom-menu-container', CustomMenuContainer)

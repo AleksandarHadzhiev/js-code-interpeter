0ce01661c5e3c106e0114c0268cd247dbc9ec27f
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-15 22:03:18 +0200

    ref: entirely operating on turnign on app based on click

--- web_ide/editor/src/classes/appRunner.js
+++ web_ide/editor/src/classes/appRunner.js
@@ -22,6 +22,10 @@ export default class AppRunner {
     }
 
     loadLines() {
-        this.linesLoader.loadLines(null)
+        this.linesLoader.loadLines(this._updateWidths)
+    }
+
+    _updateWidths() {
+        console.log("UPDATE WIDTHS")
     }
 }

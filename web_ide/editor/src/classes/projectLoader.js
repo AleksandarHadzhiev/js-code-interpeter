54e11153d861fc8bb49e6a66181d6eba5e3576ce
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-26 10:48:00 +0200

    load files dynamically

---
+++ web_ide/editor/src/classes/projectLoader.js
@@ -0,0 +1,58 @@
+export default class ProjectLoader {
+    /**
+     * 
+     * @param {String} projectName 
+     */
+    constructor(projectName) {
+        this.sidebarContent = document.getElementById('sidebar-content')
+        this.screen = document.getElementById('screen')
+        this.projectName = projectName
+    }
+
+    load() {
+        if (this.projectName.trim() !== "") {
+            this._loadContentIntoSidebar()
+        }
+    }
+
+    _loadContentIntoSidebar() {
+        const files = ['app.js', 'index.js']
+        files.forEach((file) => {
+            const fileElement = this._buildFile(file)
+            this.sidebarContent.appendChild(fileElement)
+        })
+    }
+
+    /**
+     * 
+     * @param {String} fileName 
+     * @returns {HTMLElement}
+     */
+    _buildFile(fileName) {
+        const file = document.createElement('div')
+        file.className = 'file'
+        file.id = fileName
+        file.addEventListener('click', () => {
+            this.screen.dispatchEvent(new CustomEvent(
+                "runFile", {
+                "detail": { 'fileName': fileName }
+            }
+            ))
+        })
+        const fileNameElement = this._buildFileName(fileName)
+        file.appendChild(fileNameElement)
+        return file
+    }
+
+    /**
+     * 
+     * @param {String} fileName 
+     * @returns {HTMLElement}
+     */
+    _buildFileName(fileName) {
+        const fileNameElement = document.createElement('p')
+        fileNameElement.className = 'file-name'
+        fileNameElement.textContent = fileName
+        return fileNameElement
+    }
+}

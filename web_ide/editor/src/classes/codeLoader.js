a62bc9737bafe375dccd8f9f5a11e95487d3dc6f
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-26 21:25:57 +0200

    load content on click of file

---
+++ web_ide/editor/src/classes/codeLoader.js
@@ -0,0 +1,16 @@
+import LinesLoader from "./scrollingMechanisms/LinesLoader.js";
+import ContentPicker from './contentPicker.js'
+export default class CodeLoader {
+    constructor() {
+        this.screenElement = document.getElementById('screen')
+        this.contentPicker = new ContentPicker()
+        this.minLineHeight = 28.8
+        this.maxVisibileLinesOnScreen = Math.round(this.screenElement.offsetHeight / this.minLineHeight)
+        this.linesLoader = new LinesLoader(this.maxVisibileLinesOnScreen, this.minLineHeight)
+        this.screenElement.addEventListener('loadFile', (event) => {
+            const fileName = event.detail.fileName
+            const text = this.contentPicker.pickTextFromFileWithName(fileName)
+            this.linesLoader.loadContentForText(text)
+        })
+    }
+}

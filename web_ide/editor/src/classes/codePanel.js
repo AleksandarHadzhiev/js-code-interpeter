a3340d8bd6fe002b419522a12828787cc45d0642
AleksandarHadzhiev <alekshadzhiev12@gmail.com>
2026-04-27 21:55:15 +0200

    code panel to be listener of the observer

--- web_ide/editor/src/classes/codePanel.js
+++ web_ide/editor/src/classes/codePanel.js
@@ -3,14 +3,14 @@ import CodeLoader from "./codeLoader.js";
 export default class CodePanel {
     /**
      * 
-     * @param {HTMLElement} menu 
      * @param {Number} menuWidth 
      * @param {Number} widthOfScreen 
+     * @param {HTMLElement} screen 
+     * @param {ResizeObserver} resizeOberver 
      */
-    constructor(menu, menuWidth, widthOfScreen, screen) {
-        this.screen = document.getElementById('screen')
-        this.codePanelResizer = new CodePanelResizer(menu, menuWidth, widthOfScreen, screen)
-        this.codeLoader = new CodeLoader(this.screen)
-        // for now two certain parts of the code panel are the code loader and the code panel resizer
+    constructor(menuWidth, widthOfScreen, screen, resizeOberver) {
+        this.codePanelResizer = new CodePanelResizer(menuWidth, widthOfScreen, screen)
+        this.codeLoader = new CodeLoader(screen)
+        resizeOberver.addResizeListener(this.codePanelResizer)
     }
 }
--- web_ide/editor/src/classes/codePanelResizer.js
+++ web_ide/editor/src/classes/codePanelResizer.js
@@ -1,25 +1,17 @@
 export default class CodePanelResizer {
     /**
      * 
-     * @param {HTMLElement} menuElement 
      * @param {Number} defaultMenuWidth 
      * @param {Number} screenWidth 
      * @param {HTMLElement} screen 
      */
-    constructor(menuElement, defaultMenuWidth, screenWidth, screen) {
+    constructor(defaultMenuWidth, screenWidth, screen) {
         this.content = document.getElementById('content')
         this.screenWidth = screenWidth
+        this.defaultMenuWidth = defaultMenuWidth
         this.defaultWidth = this.screenWidth - defaultMenuWidth
         this.width = this.defaultWidth
         this.leftOffset = 0
-        menuElement.addEventListener('resizing', (event) => {
-            this.leftOffset = event.detail.sidebarWidth - defaultMenuWidth
-            this.width = this.screenWidth - this.leftOffset
-            this.content.style = `
-                left: ${this.leftOffset}px;
-                width: ${this.width}px;
-            `
-        })
 
         window.addEventListener('resize', (event) => {
             this.screenWidth = screen.offsetWidth
@@ -30,5 +22,18 @@ export default class CodePanelResizer {
             `
             screen.dispatchEvent(new Event('resized'))
         })
+    }
+
+    /**
+ * 
+ * @param {Number} width 
+ */
+    updateProportions(width) {
+        this.leftOffset = width
+        this.width = this.screenWidth - this.leftOffset
+        this.content.style = `
+                left: ${this.leftOffset}px;
+                width: ${this.width}px;
+            `
     }
 }
